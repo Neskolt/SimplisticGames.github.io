ed37a5eb318c3b747c87a136e41832d52f7b73a8
Neskolt <146782778+Neskolt@users.noreply.github.com>
2023-11-13 18:48:04 -0300

    Add files via upload

---
+++ gamezone.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // Inicializar los sliders con la librería Slick
+    $(".slider-container").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
